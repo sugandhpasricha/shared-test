3ca968b53035282066dd83f1c32257ad166bc84d
Sugandh Pasricha <55180217+sugandhpasricha@users.noreply.github.com>
2021-09-13 15:29:24 +0530

    Update helloWOrld.groovy

--- vars/helloWOrld.groovy
+++ vars/helloWOrld.groovy
@@ -1,5 +1,5 @@
 def call(Map config = [:]) { 
-  def scriptcontents = libraryResource "hello.py"
-  writeFile file: "/tmp/hello.py", text: scriptcontents
-  sh "python3 /tmp/hello.py"
+ // def scriptcontents = libraryResource "hello.py"
+ // writeFile file: "/tmp/hello.py", text: scriptcontents
+  sh "python3 /var/lib/jenkins/workspace/test@libs/shared-test/resources/hello.py"
 }
