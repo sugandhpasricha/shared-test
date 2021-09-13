8f74cbf12ceb817890e9315bf1d61d4692287ac8
Sugandh Pasricha <55180217+sugandhpasricha@users.noreply.github.com>
2021-09-13 15:08:07 +0530

    Update helloWOrld.groovy

--- vars/helloWOrld.groovy
+++ vars/helloWOrld.groovy
@@ -1,5 +1,5 @@
 def call(Map config = [:]) { 
   def scriptcontents = libraryResource "hello.py"
-  writeFile file: "/tmp/hello.py", text: scriptcontents
+  writeFile file: "/tmp/hello.py", text: "print("This file was created just now")"
   sh "python3 /tmp/hello.py"
 }
